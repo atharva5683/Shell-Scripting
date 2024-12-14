2b072e414e481fbc3d5677eab9b1dc693c378f03
Ubuntu <ubuntu@ip-172-31-8-226.ap-south-1.compute.internal>
2024-12-14 19:29:14 +0000

    Update commit

--- day06/Backup-Rotation/backup.sh
+++ day06/Backup-Rotation/backup.sh
@@ -10,31 +10,38 @@ Useage:
 readme
 
 display_usage() {
-	
-	echo "Useage: ./backup.sh <path to your source> <path to backup folder>"
+    echo "Usage: ./backup.sh <path to your source> <path to backup folder>"
 }
 
 source_dir=$1
 backup_dir=$2
 timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
 
-create_backup(){	
-	
-	zip -r "$backup_dir/backup_$timestamp.zip"  "$source_dir" > /dev/null
-	if [ $? -eq 0 ]; then 
-		echo Backup generated successfully for $timestamp
-	fi
+create_backup() { 
+    zip -r "$backup_dir/backup_$timestamp.zip" "$source_dir" > /dev/null
+    if [ $? -eq 0 ]; then
+        echo "Backup generated successfully for $timestamp"
+    fi
 }
 
 perform_rotation() {
-	backups=($(ls -t "$backup_dir/backup_"*.zip  2>/dev/null))
-     	echo $backup[@]	
+    backups=($(ls -t "$backup_dir/backup_"*.zip 2>/dev/null)) 
+
+    if [[ ${#backups[@]} -gt 5 ]]; then
+        echo "Performing rotation for 5 days"
+        backup_to_remove=("${backups[@]:5}")
+
+        for backup in "${backup_to_remove[@]}"; do
+            rm -f "$backup"
+        done
+    fi
 }
 
 if [ $# -eq 0 ]; then
-	display_usage
-
+    display_usage
+    exit 1
 fi
 
 create_backup
 perform_rotation
+
